1f4da4e791a5e50221a6d3d27be70cf412338dc9
ajmalbinnizam <ajmalnizam369@gmail.com>
2020-08-25 20:06:50 +0530

    day 1

---
+++ web development/hello.js
@@ -0,0 +1,13 @@
+var name = "aju"
+
+
+var s = true
+var s1 = new Boolean(true)
+
+var name1 = new String("aju prt ltd")
+console.log(name.length)
+
+var num = new Number(10)
+console.log("hello world " + name);
+
+
