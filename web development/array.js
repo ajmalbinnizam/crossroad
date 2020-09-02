0057b8c74f6bc2d40629826eb92a04ffd8417aa2
ajmalbinnizam <ajmalnizam369@gmail.com>
2020-09-02 16:34:43 +0530

    day5

--- web development/array.js
+++ web development/array.js
@@ -2,3 +2,21 @@ var bb = ['aju', 'jech','umar', 'ajas']
 
 console.log(bb)
 
+var http = require('http')
+
+// http.createServer(server).listen(7000)
+
+// function server(req, res){
+
+//     res.write('crossroads')
+//     res.end()
+
+// }
+http.createServer(function(req,res){
+    
+    res.write('ajmalbinnziam')
+    res.end()
+
+}).listen(5000)
+
+
