80fb2cf3a6c7890d95e3b5d9d2d0990e2ab994e8
ajmalbinnizam <ajmalnizam369@gmail.com>
2020-09-07 21:44:47 +0530

    practise

--- web development/server.js
+++ web development/server.js
@@ -3,10 +3,24 @@ var fs = require('fs')
 
 http.createServer(function(req, res){
     
+        if(req.url==='/')
             
         fs.readFile('index.html',function(err,data){
         res.writeHead(200,{'content-type':'text/html'})
         res.write(data)
         res.end()
         })
-    }).listen(7000)
+
+        else if(req.url==='/login'){
+            res.write('login')
+            res.end()
+        }
+        else{
+            res.writeHead(404),{'content-type':'text/html'}
+            res.write('error')
+            res.end
+        }
+    // }).listen(7000, function(){
+    //     console.log("server started")
+    // })
+}).listen(7000, ()=> console.log("server started"))
